9e1de62fce5903536d724337e12645d2530c32ad
Muhammad Ahmad Raza <138361172+ahmad12345321@users.noreply.github.com>
2024-07-26 15:46:36 +0500

    Update textfield.dart

--- textfield.dart
+++ textfield.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -35,56 +37,142 @@ class textfield extends StatefulWidget {
 
 
 class _textfieldState extends State<textfield> {
-
-  var uname= TextEditingController();
+  var uname=TextEditingController();
+  var uemail=TextEditingController();
+  var upass=TextEditingController();
+  var uconfirm=TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        foregroundColor: Colors.cyan,
         backgroundColor: Colors.blue,
         title: Text("Textfield"),
       ),
-    body: Center(
-      child: Container(
-        child: Center(
-          child: Column(
-            children: [
-              Center(
-                child: Padding(
-                  padding: const EdgeInsets.all(30.0),
-                  child: TextField(
-                    obscureText: true,
-                    obscuringCharacter: "*",
-                    controller: uname,
-                    keyboardType: TextInputType.text,
-                    // keyboardType: TextInputType.emailAddress,
-                    // keyboardType: TextInputType.number,
-                    // enabled:false,
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.icecream),
-                      suffixIcon: IconButton(
-                        icon:Icon(Icons.remove_red_eye),
-                        onPressed: (){
-                          print("hello");
-                        },
-                      ),
-                      hintText: "Name",
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(
-                          color: Colors.yellow,
-                          width: 2
+    body:Center(
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Card(
+          shadowColor: Colors.red,
+          elevation: 20,
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+                color: Colors.blue
+            ),
+            width: 400,
+            height: 400,
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: TextField(
+                      controller: uname,
+                      keyboardType:TextInputType.text,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintText: 'Name',
+                        // suffixIcon: IconButton(
+                        //   icon: Icon(Icons.remove_red_eye,color: Colors.blueAccent,),
+                        //   onPressed: (){
+                        //     print('Raza');
+                        //   },
+                        // ),
+                        prefixIcon: Icon(Icons.drive_file_rename_outline,color: Colors.blueAccent,),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(
+                            color: Colors.pink
+                          )
                         )
                       ),
+                      ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: TextField(
+                      controller: uemail,
+                      decoration:InputDecoration(
+                        prefixIcon: Icon(Icons.email,color: Colors.blueAccent,),
+                        hintText: "Email",
+                        // suffixIcon: IconButton(
+                        //   icon: Icon(Icons.remove_red_eye,color: Colors.blueAccent,),
+                        //   onPressed: (){
+                        //     print("Email");
+                        //   },
+                        // ),
+                        fillColor: Colors.white,
+                        filled: true,
+                        border: OutlineInputBorder(
+                          borderRadius:BorderRadius.circular(10)
+                        )
+                      ) ,
                     ),
                   ),
-                ),
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: TextField(
+                      obscureText: true,
+                      obscuringCharacter: '*',
+                      controller: upass,
+                      keyboardType: TextInputType.text,
+                      decoration:InputDecoration(
+                          prefixIcon: Icon(Icons.password,color: Colors.blueAccent,),
+                          hintText: "Password",
+                          suffixIcon: IconButton(
+                            icon:Icon(Icons.remove_red_eye,color: Colors.blueAccent,),
+                            onPressed: (){
+                              print("Password");
+                            },
+                          ),
+                          fillColor: Colors.white,
+                          filled: true,
+                          border: OutlineInputBorder(
+                              borderRadius:BorderRadius.circular(10)
+                          )
+                      ) ,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: TextField(
+                      obscuringCharacter: "*",
+                      obscureText: true,
+                      controller: uconfirm,
+                      keyboardType: TextInputType.text,
+                      decoration:InputDecoration(
+                          prefixIcon: Icon(Icons.password,color: Colors.blueAccent,),
+                          hintText: "Confirm Password",
+                          suffixIcon: IconButton(
+                            icon: Icon(Icons.remove_red_eye,color: Colors.blueAccent,),
+                            onPressed: (){
+                              print("Confirm Password");
+                            },
+                          ),
+                          fillColor: Colors.white,
+                          filled: true,
+                          border: OutlineInputBorder(
+                              borderRadius:BorderRadius.circular(10)
+                          ),
+
+                      ) ,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: ElevatedButton(onPressed: (){
+                      String username=uname.text.toString();
+                      String useremail=uemail.text.toString();
+                      String userpass=upass.text.toString();
+                      String userconfirm=uconfirm.text.toString();
+                    print("Name :$username Email :$useremail Password :$userpass Confirm Password :$userconfirm");
+                    }, child: Text("Sign In")),
+                  )
+                ],
               ),
-              ElevatedButton(onPressed: (){
-                String username=uname.text.toString();
-                print("Name,$uname");
-              }, child: Text("Sign In"))
-            ],
+            ),
           ),
         ),
       ),
